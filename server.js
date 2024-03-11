aef663986f1eff662c43ea3150d96e503126718c
PrachetShah <prachetshah25@gmail.com>
2024-03-11 22:51:08 +0530

    initialised app

---
+++ server.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+const { addUser, removeUser } = require("./user");
+
+const PORT = 5000;
+
+io.on("connection", (socket) => {
+  // When user joins a room
+  socket.on("join", ({ name, room }, callBack) => {
+    const { user, error } = addUser({ id: socket.id, name, room });
+    if (error) return callBack(error);
+
+    socket.join(user.room);
+    socket.emit("message", {
+      user: "Admin",
+      text: `Welocome to ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "Admin", text: `${user.name} has joined!` });
+    callBack(null);
+
+    // Sending Message
+    socket.on("sendMessage", ({ message }) => {
+      io.to(user.room).emit("message", {
+        user: user.name,
+        text: message,
+      });
+    });
+  });
+
+  // Disconnect
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    console.log(user);
+    io.to(user.room).emit("message", {
+      user: "Admin",
+      text: `${user.name} just left the room`,
+    });
+    console.log("A disconnection has been made");
+  });
+});
+
+server.listen(PORT, () => console.log(`Server is Quannected to Port ${PORT}`));
