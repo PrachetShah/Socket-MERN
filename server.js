1be2765d0adbb0e3c99e55009dea4b1358e2f8d7
PrachetShah <prachetshah25@gmail.com>
2024-03-12 20:36:55 +0530

    Simple Connection between client and server created which emits messages

--- server.js
+++ server.js
@@ -21,15 +21,26 @@ const io = new Server(server, {
 */
 
 app.get("/", (req, res) => {
-  console.log("Home Called");
+  // console.log("Home Called");
   // res.send("App Initialised");
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("User Connected");
+  // console.log(socket.id);
+  console.log("a user connected: ", socket.id);
+
+  // for chat message
+  socket.on("chat message", (msg) => {
+    console.log("message: ", msg);
+  });
+
+  // when user disconnects
+  socket.on("disconnect", () => {
+    console.log("User with id: ", socket.id, " is disconnected");
+  });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is Connected to Port ${PORT}`);
 });
